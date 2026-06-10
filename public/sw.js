4ae942aae0051fd1a0952a28a0db6c4ba515ac37
gsoltimahler8-maker <gsoltimahler8@gmail.com>
2026-06-10 11:31:55 +0900

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+const CACHE='svns-stats-v1';
+const ASSETS=['/','/index.html','/manifest.webmanifest','/icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))));});
