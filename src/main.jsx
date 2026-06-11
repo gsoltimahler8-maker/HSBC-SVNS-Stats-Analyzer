9b1c1c55ea46dc47cec79a2b401d8a43d4b0860a
gsoltimahler8-maker <gsoltimahler8@gmail.com>
2026-06-11 12:33:22 +0900

    Import statistics helpers from utils
    Removed local implementations of pct, avg, and corr functions.

--- src/main.jsx
+++ src/main.jsx
@@ -4,33 +4,8 @@ import { createRoot } from 'react-dom/client';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ScatterChart, Scatter, Legend } from 'recharts';
 import { ShieldAlert, Database, Trophy, Filter, Info, RefreshCcw } from 'lucide-react';
 import { sampleMatches } from './data/sampleMatches.js';
+import { pct, avg, corr } from './utils/statistics.js';
 import './styles.css';
-
-function pct(n) {
-  return `${Math.round(n)}%`;
-}
-
-function avg(arr, key) {
-  return arr.length ? arr.reduce((s, m) => s + (m[key] || 0), 0) / arr.length : 0;
-}
-
-function corr(data, xKey, yKey) {
-  if (data.length < 2) return null;
-
-  const xs = data.map((d) => d[xKey]);
-  const ys = data.map((d) => d[yKey]);
-
-  const mx = xs.reduce((a, b) => a + b, 0) / xs.length;
-  const my = ys.reduce((a, b) => a + b, 0) / ys.length;
-
-  const num = xs.reduce((s, x, i) => s + (x - mx) * (ys[i] - my), 0);
-  const den = Math.sqrt(
-    xs.reduce((s, x) => s + (x - mx) ** 2, 0) *
-    ys.reduce((s, y) => s + (y - my) ** 2, 0)
-  );
-
-  return den ? num / den : null;
-}
 
 function App() {
   const seasons = [...new Set(sampleMatches.map((m) => m.season))];
