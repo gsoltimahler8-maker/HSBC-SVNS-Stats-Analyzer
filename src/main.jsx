f0d7f0ce1301f804363d1667723b19017e188525
gsoltimahler8-maker <gsoltimahler8@gmail.com>
2026-07-26 20:40:31 +0900

    Wrap App component in AppErrorBoundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import './registerSW.js';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import AppErrorBoundary from './components/AppErrorBoundary.jsx';
 import './styles.css';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <AppErrorBoundary>
+    <App />
+  </AppErrorBoundary>
+);
